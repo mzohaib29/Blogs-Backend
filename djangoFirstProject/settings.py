843abda040a7cbdca463866360d8e8b5ac363e7b
Muhammad Zohiab Tariq <mzohaibtariq860@gmail.com>
2023-07-21 15:35:52 +0500

    commit for venv packages

--- djangoFirstProject/settings.py
+++ djangoFirstProject/settings.py
@@ -144,10 +144,20 @@ WSGI_APPLICATION = 'djangoFirstProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv("DB_HOST"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PWD"),
+        'PORT': 5432,
     }
 }
 
